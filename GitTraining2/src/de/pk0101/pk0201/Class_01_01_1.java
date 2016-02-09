9515db410c8babd5f3480a51c4468dbe381dc5b4
uphamluu <uyenpham91@gmail.com>
2016-02-09 14:47:09 +0100

    change in Branch1

--- GitTraining2/src/de/pk0101/pk0201/Class_01_01_1.java
+++ GitTraining2/src/de/pk0101/pk0201/Class_01_01_1.java
@@ -10,12 +10,22 @@ public class Class_01_01_1 {
         // TODO Auto-generated method stub
         
         System.out.println("Class_01_01_1");
+//      change something in file (branch1) : expect Confict
+    //  change something in file (branch1) : expect Confict
+    //  change something in file (branch1) : expect Confict
 
     }
     
     public static void dosomething() {
         
         System.out.println("dosomething");
+//      change something in file (branch1) : expect Confict
+    //  change something in file (branch1) : expect Confict
+    //  change something in file (branch1) : expect Confict
     }
+    
+//    change something in file (branch1) : expect Confict
+//  change something in file (branch1) : expect Confict
+//  change something in file (branch1) : expect Confict
 
 }
