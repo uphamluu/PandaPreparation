ac962b6ad7dc3e8f3764da4513b3a144e67e73bf
uphamluu <uyenpham91@gmail.com>
2016-02-09 13:17:10 +0100

    first share Projectin Repo

---
+++ GitTraining_bck/src/de/pk0102/pk0201/Class_02_01_01.java
@@ -0,0 +1,17 @@
+package de.pk0102.pk0201;
+
+public class Class_02_01_01 {
+
+    // some info of class
+ // some info of class
+ // some info of class
+ // some info of class
+    
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+        System.out.println("Class_01_1");
+
+    }
+
+}
